e2ca9c9a0bcc5b6f4afca0c34490c759aa1990be
manish-bnt <manishbasnet500@gmail.com>
2025-12-19 22:30:08 +0530

    Update email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -3,9 +3,10 @@ const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL,
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 })
+
 
 module.exports = transporter
